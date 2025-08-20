25a0a3ba483baade87fd8b2d8cd17add30e7d234
Brittany Tylutke <tylub001@gmail.com>
2025-08-20 18:03:54 -0400

    Bind to Railway port

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+
+port = int(os.environ.get("PORT", 5000))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
+
 @app.route("/")
 def homepage():
     return render_template("index.html")
